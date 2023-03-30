344a5bacc60a59f791e1d9889e3c3be6027d85bd
Chay-May <chayenne.mayara@gmail.com>
2023-03-29 21:06:10 -0300

    aprofundamento-sql-chay-c

--- database/pratica-aprofundamento-sql.sql
+++ database/pratica-aprofundamento-sql.sql
@@ -1,4 +1,7 @@
+-- Active: 1680128745866@@127.0.0.1@3306
 -- Conecte o arquivo pratica-aprofundamento-sql.db com a extensão MySQL e ative a conexão aqui
+
+
 
 -- Deletar tabela
 DROP TABLE pokemons;
@@ -43,3 +46,50 @@ VALUES
 SELECT * FROM pokemons;
 
 -- Práticas
+
+-- PRÁTICA I
+
+SELECT * FROM pokemons 
+WHERE speed > 60;
+
+SELECT * FROM pokemons 
+WHERE attack >= 60 
+AND special_attack >= 60;
+
+SELECT * FROM pokemons
+WHERE name LIKE "%saur"
+
+
+--PRÁTICA GUIADA 2
+
+SELECT * FROM pokemons;
+
+SELECT AVG(hp) AS mediaSimples FROM pokemons; --MÉDIA SIMPLES
+
+-- ROUND() -ARREDONDA VALORES
+
+SELECT COUNT(*) FROM pokemons; --NÚMERO DE LINHAS
+
+SELECT COUNT(*) AS numeroLinhasDaTabela FROM pokemons; --AS - NOMEIA RETORNO
+
+SELECT SUM(hp) AS somaDaColunaHP FROM pokemons; -- SUM - SOMA ELEMENTOS DA TABELA
+
+-- PRÁTICA III
+
+SELECT * FROM pokemons  ORDER BY defense DESC; -- DESC - ORDENA DECRESCENTE
+
+SELECT * FROM pokemons ORDER BY defense ASC; -- ASC - ORDENA CRESCENTE
+
+SELECT * FROM pokemons GROUP BY type; -- GROUP - AGRUPA
+
+SELECT COUNT(*) AS quantidadeDePokemons, type FROM pokemons GROUP BY type;
+
+SELECT * FROM pokemons LIMIT 3 OFFSET 4; -- LIMIT -LIMITA VISUALIZAÇÃO DE REGISTRO -- OFFSET - DIZ A PARTIR DE QUE LINHA
+
+-- EXERCÍCIO DE FIXAÇÃO
+
+SELECT * FROM pokemons -- RETORNA TABELA
+WHERE type = "grass" OR type = "fire" -- RETORNA TIPO
+ORDER BY attack ASC -- ORDENAÇÃO CRESCENTE
+LIMIT 3 OFFSET 2; -- LIMITAR A QUANTIDADE DE REGISTROS
+
